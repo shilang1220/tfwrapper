70cbcfbd528a7bc6e172d3f5f7b7680757dae289
shilang1220 <pgl@pku.edu.cn>
2018-05-12 10:58:20 +0800

    Filled alexnet.

---
+++ test/test_VGG16.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-5-12 上午8:48
+# @Author  : Guoliang PU
+# @File    : test_VGG16.py
+from unittest import TestCase
+
+
+# @Software: tfwrapper
+class TestVGG16(TestCase):
+    def test_VGG16(self):
+        self.fail()
+
+
+
+# @Software: tfwrapper
+class TestAlexnet(TestCase):
+    def test_Alexnet(self):
+        self.fail()
